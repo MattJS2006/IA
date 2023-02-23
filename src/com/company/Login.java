c0699a9038591f22abec0f352c9f37683ca1b40e
MattJS2006 <17swann_m@bexleygs.co.uk>
2023-02-23 11:44:47 +0000

    Finished Login binary search recursion and start of merge sort.

--- src/com/company/Login.java
+++ src/com/company/Login.java
@@ -1,14 +1,14 @@
 package com.company;
-/*
+
 public class Login {
 
     public boolean binarySearchRecursion(String[] data, String n, int startPos, int endPos){
         int mid = (startPos+endPos)/2;
-        if (data[mid] == n){
+        if (data[mid].equals(n)){
             return true;
-        } else if(data[mid] > n){
+        } else if(data[mid].compareTo(n) > 0){
             return binarySearchRecursion(data,n,startPos,mid);
-        } else if(data[mid] < n){
+        } else if(data[mid].compareTo(n) < 0){
             return binarySearchRecursion(data,n,mid,endPos);
         }
         return false;
@@ -18,4 +18,4 @@ public class Login {
         String[] array =
     }
 }
- */
+
