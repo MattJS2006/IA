40b6a56079f2f34bf99ab036b395cdf8932bb24a
MattJS2006 <118258717+MattJS2006@users.noreply.github.com>
2023-03-30 12:26:47 +0100

    Create Level.java

---
+++ src/com/company/Level.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Level {
+  
+  private String current;
+  private ArrayList<String> levels = new ArrayList<String>;
+  
+  public Level(){
+    current = null;
+    setLevels;
+  }
+  
+  public Level(String current){
+    this.current = current;
+    setLevels;
+  }
+  
+  public void setLevels(){
+    levels.add("Beginner");
+    levels.add("Development");
+    levels.add("Seasoned Athlete");
+    levels.add("Adult Development");
+    levels.add("Young Elite");
+    levels.add("Adult Elite");
+  
+  public Level getLevel(){
+    return current;
+  }
+  
+  public void setLevel(String newLevel){
+    current = newLevel;
+    System.out.println("The new level is: " + current)
+    }
+  
+  public void displayLevel(){
+    System.out.println("Your current level is: " + current)
+    }
+  
+  public boolean correctLevel(){
+    for(int i = 0; i < levels.length(); i++){
+      if (levels[i] == current){
+        return true;
+      }
+    }
+    return false;
+    
