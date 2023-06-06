741c74bae65c8bef08bfceaf42ad181a59b412f1
MattJS2006 <17swann_m@bexleygs.co.uk>
2023-06-06 09:25:44 +0100

    correcting date problems in programme

--- src/com/company/Programme.java
+++ src/com/company/Programme.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
 public class Programme {
@@ -11,12 +13,20 @@ public class Programme {
         LocalDate startDate = LocalDate.now(); // Set the start date as today
         String line;
         int day = 1;
+        DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
 
-        while ((FileHandler.readWholeLineAt(filename,day-1)) != null) {
+        while (day-1 < 7) {
             LocalDate currentDate = startDate.plusDays(day - 1); // Calculate the current date
 
-            System.out.println("Day " + day + ": " + currentDate);
-            System.out.println(FileHandler.readWholeLineAt(filename,day-1));
+            System.out.println(currentDate + ":");
+            line = FileHandler.readFromFile(filename,day-1);
+            String[] parts = line.split(": ");
+            String date = dateFormat.format(currentDate);
+            if(parts[0] == date){
+                System.out.println("Rest day / Personal Training");
+            } else {
+                System.out.println(line);
+            }
             System.out.println();
             day++;
         }
